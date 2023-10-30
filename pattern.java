6dca8837b7d54e82c3432e334479e8f113aeb1d9
Ayushghagre <148415018+Ayushghagre@users.noreply.github.com>
2023-10-30 22:04:58 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 {   
 
 int i, j, row=6;   
-System.out.println("Right Angled ");
+System.out.println("Right Angled Triangle ");
 for(i=0; i<row; i++)   
 {   
  
